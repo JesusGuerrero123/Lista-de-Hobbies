e2e13356dbd71058414dfc8c66bf60efd7b04752
JesusGuerrero <hp@equipo2.com>
2023-03-01 15:12:21 -0600

    mi lista de hobbies

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+    function App() {
+      const name = "Jesus Guerrero";
+      return(
+        <div>
+        <h3> Lista de hobbies - {name}</h3>
+        <ul>
+        <li> tocar guitarra </li>
+        <li> jugar videojuegos </li>
+        <li> comer </li>
+        </ul>
+        </div>)
+      
+    }
+  
+
+export default App
